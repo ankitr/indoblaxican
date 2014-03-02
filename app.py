35e21b76c359e71235ff03424a24bed4ecfc034d
Ankit Ranjan <me@ankit.io>
2014-03-02 15:05:48 -0800

    Introduced hrs and languages.

--- app.py
+++ app.py
@@ -4,9 +4,17 @@ from flask import render_template
 
 app = Flask(__name__, static_folder='static', static_url_path='')
 
-@app.route("/")
+@app.route('/')
 def hello():
     return render_template('hello.html')
+
+@app.route('/black')
+def black():
+    return render_template('black.html')
+
+@app.route('/indo')
+def indo():
+    return render_template('indo.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
