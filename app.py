f5917f35b849f1aeb2c331511888e83993c55e08
Ankit Ranjan <me@ankit.io>
2014-03-01 21:49:37 -0800

    Setup rudimentary deploy script and some content

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from flask import Flask
+
+from flask import render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Welcome to the future home of the Indoblaxican."
+    return render_template('hello.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo 'Deploying the Indo-Blaxican.'
+echo 'Stuffing curry-fed fried chickens into tacos.'
+python app.py
